d035c53f4901244a7fbb82af81ca115f45ed127c
ManuelNavarro <00503013@uca.edu.sv>
2019-01-05 10:27:40 -0600

    pantalla manteniento ubicacion 05012018

--- inventario/models.py
+++ inventario/models.py
@@ -2,56 +2,56 @@ from django.db import models
 
 # Create your models here.
 class Unidad(models.Model):
-	idUnidad = models.IntegerField(primary_key = True)
+	idUnidad = models.AutoField(primary_key = True)
 	codigoUnidad = models.CharField(max_length = 10)
 	nombreUnidad = models.CharField(max_length = 75)
 	activo = models.BooleanField(default = True)
 	
 class PuestoUnidad(models.Model):
-	idPuestoUnidad = models.IntegerField(primary_key = True)
+	idPuestoUnidad = models.AutoField(primary_key = True)
 	puestoUnidadClaseId = models.IntegerField()
 	nombrePuestoUnidad = models.CharField(max_length = 50)
 	puestoJefeId = models.IntegerField()
 	idUnidad = models.ForeignKey(Unidad, on_delete = models.DO_NOTHING)
 
 class Plaza(models.Model):
-	idPlaza = models.IntegerField(primary_key = True)
+	idPlaza = models.AutoField(primary_key = True)
 	nombrePlaza = models.CharField(max_length = 50)
 	activo = models.BooleanField(default = True)
 	idUnidad = models.ForeignKey(Unidad, on_delete = models.DO_NOTHING)
 	
 class Persona(models.Model):
-	idPersona = models.IntegerField(primary_key = True)
+	idPersona = models.AutoField(primary_key = True)
 	nombrePersona = models.CharField(max_length = 50)
 	apellidoPersona = models.CharField(max_length = 50)
 	
 class Empleado(models.Model):
-	idEmpleado = models.IntegerField(primary_key = True)
+	idEmpleado = models.AutoField(primary_key = True)
 	codigoEmpleado = models.CharField(max_length = 7)
 	activo = models.BooleanField(default = True)
 	idPersona = models.ForeignKey(Persona, on_delete = models.DO_NOTHING)
 
 #Tabla Plaza_x_Empleado
 class PlazaEmpleado(models.Model):
-	idPlazaEmpleado = models.IntegerField(primary_key = True)
+	idPlazaEmpleado = models.AutoField(primary_key = True)
 	plaza = models.ForeignKey(Plaza, on_delete = models.DO_NOTHING)
 	idEmpleado = models.ForeignKey(Empleado, on_delete = models.DO_NOTHING)
 	activo = models.BooleanField(default = True)
 
 class EstadoPrestamo(models.Model):
-	idEstadoPrestamo = models.IntegerField(primary_key = True)
+	idEstadoPrestamo = models.AutoField(primary_key = True)
 	nombreEstadoPrestamo = models.CharField(max_length = 50)
 	descripcionEstadoPrestamo = models.CharField(max_length= 200)
 	activo = models.BooleanField(default = True)
 	
 class EstadoTransferencia(models.Model):
-	idEstadoTransferencia = models.IntegerField(primary_key = True)
+	idEstadoTransferencia = models.AutoField(primary_key = True)
 	nombreEstadoTransferencia = models.CharField(max_length = 50)
 	descripcionEstadoTransferencia = models.CharField(max_length= 200)
 	activo = models.BooleanField(default = True)
 	
 class Prestamo(models.Model):
-	idPrestamo = models.IntegerField(primary_key = True)
+	idPrestamo = models.AutoField(primary_key = True)
 	codigoPrestamo = models.CharField(max_length = 25)
 	motivoPrestamo = models.CharField(max_length = 200)
 	fechaPrestamo = models.DateField()
@@ -62,7 +62,7 @@ class Prestamo(models.Model):
 	idEmpleado = models.ForeignKey(Empleado, on_delete = models.DO_NOTHING)
 	
 class Transferencia(models.Model):
-	idTransferencia = models.IntegerField(primary_key = True)
+	idTransferencia = models.AutoField(primary_key = True)
 	codigoTransferencia = models.CharField(max_length = 25)
 	fechaTransferencia = models.DateField()
 	motivoTransferencia = models.CharField(max_length = 200)
@@ -73,7 +73,7 @@ class Transferencia(models.Model):
 	idEmpleado = models.ForeignKey(Empleado, on_delete = models.DO_NOTHING)
 	
 class Serie(models.Model):
-	idSerie = models.IntegerField(primary_key = True)
+	idSerie = models.AutoField(primary_key = True)
 	nombreSerie = models.CharField(max_length = 200)
 	DescripcionSerie = models.CharField(max_length = 200)
 	codSerie = models.CharField(max_length = 10)
@@ -81,7 +81,7 @@ class Serie(models.Model):
 	idSeriePadre = models.IntegerField()
 	
 class Expediente(models.Model):
-	idExpediente = models.IntegerField(primary_key = True)
+	idExpediente = models.AutoField(primary_key = True)
 	codExpediente = models.IntegerField()
 	nombreExpediente = models.CharField(max_length = 100)
 	DescripcionExpediente = models.CharField(max_length = 200)
@@ -96,59 +96,59 @@ class Expediente(models.Model):
 	idTransferencia = models.ForeignKey(Transferencia, on_delete = models.DO_NOTHING)
 	
 class PrestamoExpediente(models.Model):
-	idPrestamoExpediente = models.IntegerField(primary_key = True)
+	idPrestamoExpediente = models.AutoField(primary_key = True)
 	idPrestamo = models.ForeignKey(Prestamo, on_delete = models.DO_NOTHING)
 	idExpediente = models.ForeignKey(Expediente, on_delete = models.DO_NOTHING)
 	devuelto = models.BooleanField(default = False)
 	
 class SerieExpediente(models.Model):
-	idSerieExpediente = models.IntegerField(primary_key = True)
+	idSerieExpediente = models.AutoField(primary_key = True)
 	esPrincipal = models.BooleanField(default = False)
 	idExpediente = models.ForeignKey(Expediente, on_delete = models.DO_NOTHING)
 	idSerie = models.ForeignKey(Serie, on_delete = models.DO_NOTHING)
 	
 class Zona(models.Model):
-	idZona = models.IntegerField(primary_key = True)
+	idZona = models.AutoField(primary_key = True)
 	nombreZona = models.CharField(max_length = 10)
 	descripcionZona = models.CharField(max_length = 150)
 	activo = models.BooleanField(default = True)
 	
 class Carro(models.Model):
-	idCarro = models.IntegerField(primary_key = True)
+	idCarro = models.AutoField(primary_key = True)
 	nombreCarro = models.CharField(max_length = 10)
 	descripcionCarro = models.CharField(max_length = 150)
 	activo = models.BooleanField(default = True)
 	idZona = models.ForeignKey(Zona, on_delete = models.DO_NOTHING)
 	
 class Estante(models.Model):
-	idEstante = models.IntegerField(primary_key = True)
+	idEstante = models.AutoField(primary_key = True)
 	nombreEstante = models.CharField(max_length = 10)
 	descripcionEstante = models.CharField(max_length = 150)
 	activo = models.BooleanField(default = True)
 	idCarro = models.ForeignKey(Carro, on_delete = models.DO_NOTHING)
 
 class Bandeja(models.Model):
-	idBandeja = models.IntegerField(primary_key = True)
+	idBandeja = models.AutoField(primary_key = True)
 	nombreBandeja = models.CharField(max_length = 10)
 	descripcionBandeja = models.CharField(max_length = 150)
 	activo = models.BooleanField(default = True)
 	idEstante = models.ForeignKey(Estante, on_delete = models.DO_NOTHING)
 	
 class Caja(models.Model):
-	idCaja = models.IntegerField(primary_key = True)
+	idCaja = models.AutoField(primary_key = True)
 	nombreCaja = models.CharField(max_length = 10)
 	descripcionCaja = models.CharField(max_length = 150)
 	activo = models.BooleanField(default = True)
 	idBandeja = models.ForeignKey(Bandeja, on_delete = models.DO_NOTHING)
 	
 class EstadoExpediente(models.Model):
-	idEstadoExpediente = models.IntegerField(primary_key = True)
+	idEstadoExpediente = models.AutoField(primary_key = True)
 	nombreEstadoExpediente = models.CharField(max_length = 50)
 	descripcionEstadoExpediente = models.CharField(max_length= 200)
 	activo = models.BooleanField(default = True)
 
 class ExpedienteCaja(models.Model):
-	idExpedienteCaja = models.IntegerField(primary_key = True)
+	idExpedienteCaja = models.AutoField(primary_key = True)
 	idExpediente = models.ForeignKey(Expediente, on_delete = models.DO_NOTHING)
 	idCaja = models.ForeignKey(Caja, on_delete = models.DO_NOTHING)
 	idEstadoExpediente = models.ForeignKey(EstadoExpediente, on_delete = models.DO_NOTHING)
--- inventario/urls.py
+++ inventario/urls.py
@@ -1,7 +1,33 @@
 from django.conf.urls import url, include
 
 from inventario.views import index
+from inventario.mantenimiento_ubicacion_views import MantenimientoUbicacionViews
+from inventario.mantenimiento_ubicacion_views import obtenerComboZonas
+from inventario.mantenimiento_ubicacion_views import llenarComboCarro
+from inventario.mantenimiento_ubicacion_views import llenarComboEstante
+from inventario.mantenimiento_ubicacion_views import dt_ZonaDT
+from inventario.mantenimiento_ubicacion_views import dt_CarroDT
+from inventario.mantenimiento_ubicacion_views import dt_EstanteDT
+from inventario.mantenimiento_ubicacion_views import dt_BandejaDT
+from inventario.mantenimiento_ubicacion_views import editarZona
+from inventario.mantenimiento_ubicacion_views import crearZona
+from inventario.mantenimiento_ubicacion_views import editarCarro
+from inventario.mantenimiento_ubicacion_views import crearCarro
 
 urlpatterns = [
     url(r'^index$', index),
+
+    #Mantenimiento de ubicacion
+    url(r'^mantenimiento_ubicacion/', MantenimientoUbicacionViews, name='MantenimientoUbicacionViews'),
+    url(r'^obtenerComboZonas/', obtenerComboZonas, name='obtenerComboZonas'),
+    url(r'^llenarComboCarro/', llenarComboCarro, name='llenarComboCarro'),
+    url(r'^llenarComboEstante/', llenarComboEstante, name='llenarComboEstante'),
+    url(r'^dt_ZonaDT/', dt_ZonaDT, name='dt_ZonaDT'),
+    url(r'^dt_CarroDT/', dt_CarroDT, name='dt_CarroDT'),
+    url(r'^dt_EstanteDT/', dt_EstanteDT, name='dt_EstanteDT'),
+    url(r'^dt_BandejaDT/', dt_BandejaDT, name='dt_BandejaDT'),
+    url(r'^editarZona/', editarZona, name='editarZona'),
+    url(r'^crearZona/', crearZona, name='crearZona'),
+    url(r'^editarCarro/', editarCarro, name='editarCarro'),
+    url(r'^crearCarro/', crearCarro, name='crearCarro'),
 ]
